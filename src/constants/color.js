9185b58881c053fe33a1553af86e3cdf44204a99
vasantharuban19 <vasanthruban1920@gmail.com>
2024-04-03 15:00:26 +0530

    updated

--- src/constants/color.js
+++ src/constants/color.js
@@ -7,10 +7,11 @@ export const grayColor = "rgba(247,247,247,1)";
 export const lightBlue = "#2694ab";
 export const matBlack = "rgba(0,0,0,0.8)";
 export const bgGradient =
-  "linear-gradient( #E5FFCC, #1EBEA5, #00E676, #D0E9EA, #EDF8F5, #ffffff)";
+  "linear-gradient( #E5FFCC, #1EBEA5, #00E676, #D0E9EA)";
 export const purple = "rgba(75,12,192,1)";
 export const LightPurple = "rgba(75,12,192,0.2)";
 
 
 
 // 'linear-gradient( rgb(255 225 209), rgb(249 159 159))'
+
